ca0fb9e73600f29e1ceaecd200ba3ccb8b9f483d
annettebowland <annettebowland@yahoo.com>
2025-03-30 21:17:48 +0000

    Change UI - refactoring code

--- src/standard-configuration/standardIcon.ts
+++ src/standard-configuration/standardIcon.ts
@@ -1,13 +1,13 @@
 import { computeDomain, HomeAssistant, stateIcon } from 'custom-card-helpers';
 import { HassEntity } from 'home-assistant-js-websocket';
 
-export const binarySensorIcon = (stateObj: HassEntity) => {
+export let binarySensorIcon = (stateObj: HassEntity) => {
   if (stateObj) return stateIcon({ ...stateObj, state: 'off' }) || 'mdi:radiobox-blank';
   else return 'mdi:radiobox-blank';
 };
 
-export const sensorIcon = (stateObj: HassEntity) => {
-  const deviceClass = stateObj.attributes.device_class || '';
+export let sensorIcon = (stateObj: HassEntity) => {
+  let deviceClass = stateObj.attributes.device_class || '';
   switch (deviceClass) {
     case 'humidity':
       return 'mdi:water-percent';
@@ -26,8 +26,8 @@ export const sensorIcon = (stateObj: HassEntity) => {
   }
 };
 
-const coverIcon = (stateObj: HassEntity, state?: string) => {
-  const closedState = state == 'closed';
+let coverIcon = (stateObj: HassEntity, state?: string) => {
+  let closedState = state == 'closed';
   switch (stateObj.attributes.device_class) {
     case 'garage':
       return closedState ? 'mdi:garage' : 'mdi:garage-open';
@@ -42,7 +42,7 @@ const coverIcon = (stateObj: HassEntity, state?: string) => {
   }
 };
 
-export const domainIcons: Record<string, string> = {
+export let domainIcons: Record<string, string> = {
   alarm_control_panel: 'mdi:alarm-light-outline',
   automation: 'mdi:playlist-play',
   binary_sensor: 'mdi:radiobox-blank',
@@ -80,9 +80,9 @@ export const domainIcons: Record<string, string> = {
   water_heater: 'mdi:water-boiler',
 };
 
-export const standardIcon = (entity_id: string, hass: HomeAssistant) => {
-  const domain = computeDomain(entity_id);
-  const stateObj = hass.states[entity_id];
+export let standardIcon = (entity_id: string, hass: HomeAssistant) => {
+  let domain = computeDomain(entity_id);
+  let stateObj = hass.states[entity_id];
 
   switch (domain) {
     case 'binary_sensor':
